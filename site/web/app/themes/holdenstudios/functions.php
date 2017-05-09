76cbbe0ab48c4542526fe4ef9789ce97a549889f
Mike Walcott <mikewalcott@Mikes-MacBook-Pro.local>
2017-05-09 11:22:11 -0600

    adjusting links for products on product category pages

--- site/web/app/themes/holdenstudios/functions.php
+++ site/web/app/themes/holdenstudios/functions.php
@@ -43,8 +43,8 @@ remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_singl
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
 
 
-remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
-remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
+//remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+//remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
 
 //remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
 
