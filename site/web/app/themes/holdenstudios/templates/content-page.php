a7fe2c2c25a858d4097ef955859f890a87327562
Mike <m.walcott33@gmail.com>
2017-06-06 16:56:59 -0600

    updating wholesale page

--- site/web/app/themes/holdenstudios/templates/content-page.php
+++ site/web/app/themes/holdenstudios/templates/content-page.php
@@ -1,17 +1,12 @@
 <?php if(is_page(100)) { ?>
 	<div class="row">
 		<div class="col-sm-12">
-			<p class="wholesale">Interested in selling our products? Contact us via email at <a href="mailto:holdenstudiosdenver@gmail.com">holdenstudiosdenver@gmail.com</a> or use the form below to enquire.</p>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-sm-5">
 			<img src="<?php the_field('banner_background_image_mobile'); ?>" class="img-responsive"/>
-		</div>
-		<div class="col-sm-7">
-			<?php the_content(); ?>			
+<!-- 			<p class="wholesale">Interested in selling our products? Contact us via email at <a href="mailto:holdenstudiosdenver@gmail.com">holdenstudiosdenver@gmail.com</a> or use the form below to enquire.</p> -->
 		</div>
 	</div>
+	
+	<?php the_content(); ?>
 
 <?php } else { ?>
 	<?php the_content(); ?>
