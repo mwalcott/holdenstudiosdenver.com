339e04131d4fbb712c95e8647faa4cdbd6fac3a2
Mike Walcott <mikewalcott@Mikes-MacBook-Pro.local>
2018-05-02 15:51:30 -0600

    adjustments to template

--- site/web/app/themes/holdenstudios/functions.php
+++ site/web/app/themes/holdenstudios/functions.php
@@ -40,8 +40,12 @@ foreach ( $terms as $term ) $categories[] = $term->slug;
 
 if ( in_array( 'ornaments', $categories ) || in_array( 'coasters', $categories ) || in_array( 'magnets', $categories ) ) {
   echo '<p class="ships-free"><strong>Ships within 7-10 business days.</strong></p>';	
-} else {
-  echo '<p class="ships-free"><strong>Ships FREE within 7-10 business days.</strong></p>';	
+} elseif( in_array( 'cornhole', $categories ) ) {
+	echo '';	
+}	else {
+  echo '<p>All artwork is ready to hang</p>';
+  echo '<p class="ships-free"><strong>All orders over $40 ship free. Ships within 7-10 business days.</strong><br />';	
+  echo '<i style="font-size: 1rem;">*excludes corhole sets</i></p>';
 }
 	
 /*
