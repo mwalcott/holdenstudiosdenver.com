4e7ccaa8d2ca43c70b1131ecaa3f26ed9cd883d4
Mike Walcott <mikewalcott@Mikes-MacBook-Pro.local>
2018-06-14 11:29:33 -0600

    added text to wedding page

--- site/web/app/themes/holdenstudios/templates/content-page.php
+++ site/web/app/themes/holdenstudios/templates/content-page.php
@@ -16,7 +16,7 @@
 	if (strpos($url,'2187') !== false) {
 		//echo '<p class="wedding-text" style="text-align: center;"><strong>Wedding season is upon us!!</strong><br /><br />We love weddings and marriage! Let Holden Studios help with a stunning hand burned guestbook for your special day. Have your guests sign the back for memories that last a lifetime.</p>';
 		echo '<h3 style="text-align: center;">Customize Every Detail</h3>';
-		echo '<p class="wedding-text" style="text-align: center;">Our exceptional service, quality products, and attention to every detail are what have made Holden Studios Colorado’s number one resource for hand - crafted, hand burned, hand stained wedding guestbook flags. Each flag is coated with an outdoor finish and ready to hang. Want your own design added? Give us a call and we will get it exactly the way you want it for memories to last a lifetime.</p>';
+		echo '<p class="wedding-text" style="text-align: center;">Our exceptional service, quality products, and attention to every detail are what have made Holden Studios Colorado’s number one resource for hand - crafted, hand burned, hand stained wedding guestbook flags. Each flag is coated with an outdoor finish and ready to hang. Want your own design added? Give us a call and we will get it exactly the way you want it for memories to last a lifetime.</p><p>If desired, you can ask your guests to sign the back with standard sharpie.” After ready to hang.</p>';
 
 		echo '<h3 style="text-align: center; margin-top: 0; margin-bottom:0;">Contact Us</h3>';
 		echo '<h3 style="text-align: center; margin-top: 0;"><a href="tel:7203605090">720 360 5090</a></h3>';
